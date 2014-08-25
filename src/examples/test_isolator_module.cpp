57310e50f597614ffed626d7261c1be56e334048
Niklas Q. Nielsen <niklas@mesosphere.io>
2014-08-25 15:36:48 -0700

    Added missing sources

---
+++ src/examples/test_isolator_module.cpp
@@ -0,0 +1,151 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <list>
+#include <string>
+
+#include <process/dispatch.hpp>
+#include <process/future.hpp>
+#include <process/owned.hpp>
+#include <process/process.hpp>
+
+#include <stout/try.hpp>
+
+#include <tests/module.hpp>
+
+#include <modules/isolator_module.hpp>
+
+#include <slave/containerizer/isolator.hpp>
+
+using namespace process;
+
+namespace mesos {
+namespace internal {
+namespace slave {
+
+class TestIsolatorImpl : public IsolatorModule
+{
+public:
+  TestIsolatorImpl(process::Owned<IsolatorProcess> process)
+    : IsolatorModule(process)
+  {
+  }
+  
+  virtual process::Future<Nothing> recover(
+      const std::list<state::RunState>& state) {
+    return Nothing();
+  }
+
+  virtual process::Future<Option<CommandInfo> > prepare(
+      const ContainerID& containerId,
+      const ExecutorInfo& executorInfo)
+  {
+    return None();
+  }
+
+  virtual process::Future<Nothing> isolate(
+      const ContainerID& containerId,
+      pid_t pid)
+  {
+    return Nothing();
+  }
+
+  virtual process::Future<Limitation> watch(
+      const ContainerID& containerId)
+  {
+    return Limitation(Resource(), "");
+  }
+
+  virtual process::Future<Nothing> update(
+      const ContainerID& containerId,
+      const Resources& resources)
+  {
+    return Nothing();
+  }
+
+  virtual process::Future<ResourceStatistics> usage(
+      const ContainerID& containerId)
+  {
+    return ResourceStatistics();
+  }
+
+  virtual process::Future<Nothing> cleanup(const ContainerID& containerId)
+  {
+    return Nothing();
+  }
+};
+
+class TestIsolatorProcess : public IsolatorProcess
+{
+public:
+  virtual process::Future<Nothing> recover(
+      const std::list<state::RunState>& state) {
+    return Nothing();
+  }
+
+  virtual process::Future<Option<CommandInfo> > prepare(
+      const ContainerID& containerId,
+      const ExecutorInfo& executorInfo)
+  {
+    return None();
+  }
+
+  virtual process::Future<Nothing> isolate(
+      const ContainerID& containerId,
+      pid_t pid)
+  {
+    return Nothing();
+  }
+
+  virtual process::Future<Limitation> watch(
+      const ContainerID& containerId)
+  {
+    return Limitation(Resource(), "");
+  }
+
+  virtual process::Future<Nothing> update(
+      const ContainerID& containerId,
+      const Resources& resources)
+  {
+    return Nothing();
+  }
+
+  virtual process::Future<ResourceStatistics> usage(
+      const ContainerID& containerId)
+  {
+    return ResourceStatistics();
+  }
+
+  virtual process::Future<Nothing> cleanup(const ContainerID& containerId)
+  {
+    return Nothing();
+  }
+};
+
+} // namespace slave {
+} // namespace internal {
+} // namespace mesos {
+
+extern "C" {
+void* create_isolator(void *args) {
+  process::Owned<mesos::internal::slave::IsolatorProcess> process(
+      new mesos::internal::slave::TestIsolatorProcess);
+  return new mesos::internal::slave::TestIsolatorImpl(process);
+}
+};
+
