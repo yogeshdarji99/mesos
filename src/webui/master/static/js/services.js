119f7fcd18e55aea53dedaaefebd1f6a050b811e
mlunoe <michael.lunoe@gmail.com>
2015-05-14 15:05:23 -0700

    Mesos UI: Change paths, pailer, and logs to work with a reverse proxy.
    Includes correcting routes to be /slave/{id}...
    Redirect from old mesos ui location to new dcos mesos ui location.
    
    Rebased on top of 0.27.0

--- src/webui/master/static/js/services.js
+++ src/webui/master/static/js/services.js
@@ -270,7 +270,7 @@
       return;
     }
 
-    this.endpoint = '//' + host + '/monitor/statistics?jsonp=JSON_CALLBACK';
+    this.endpoint = host + 'monitor/statistics?jsonp=JSON_CALLBACK';
     this.scope = scope;
 
     // Initial poll is immediate.
