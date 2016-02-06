83580db6ddba80a8e9a27e17b60d12032236c7f7
Michael Park <mpark@apache.org>
2016-02-05 22:58:16 -0800

    Added callgrind segments.

--- src/master/http.cpp
+++ src/master/http.cpp
@@ -54,6 +54,8 @@
 #include <stout/try.hpp>
 #include <stout/utils.hpp>
 
+#include <valgrind/callgrind.h>
+
 #include "common/build.hpp"
 #include "common/http.hpp"
 #include "common/protobuf_utils.hpp"
@@ -894,6 +896,9 @@ string Master::Http::STATE_HELP()
 
 Future<Response> Master::Http::state(const Request& request) const
 {
+  CALLGRIND_START_INSTRUMENTATION;
+  CALLGRIND_ZERO_STATS;
+
   JSON::Object object;
   object.values["version"] = MESOS_VERSION;
 
@@ -1026,7 +1031,12 @@ Future<Response> Master::Http::state(const Request& request) const
     object.values["unregistered_frameworks"] = std::move(array);
   }
 
-  return OK(object, request.url.query.get("jsonp"));
+  auto ok = OK(object, request.url.query.get("jsonp"));
+
+  CALLGRIND_DUMP_STATS;
+  CALLGRIND_STOP_INSTRUMENTATION;
+
+  return std::move(ok);
 }
 
 
